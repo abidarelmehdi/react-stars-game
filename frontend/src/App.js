1b2def117d6f0e790fef1d12218b249db0d722af
WORK@DELL <abidar.mehdi95@gmail.com>
2020-04-05 15:48:41 +0100

    add GithubList component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import GithubItem from "./components/GithubCard/GithubItem";
+import GithubList from "./components/GithubCard/GithubList";
 export class App extends Component {
   render() {
-    return <GithubItem />;
+    return <GithubList />;
   }
 }
 
--- frontend/src/components/GithubCard/GithubItem.jsx
+++ frontend/src/components/GithubCard/GithubItem.jsx
@@ -1,28 +1,48 @@
 import React, { Component } from "react";
+import Moment from "react-moment";
 
 export class GithubItem extends Component {
   render() {
     return (
-      <div>
-        <a
-          href="#"
-          className="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out"
-        >
-          <div className="flex items-center px-4 py-4 sm:px-6">
-            <div className="min-w-0 flex-1 flex items-center">
-              <div className="flex-shrink-0">
-                <img
-                  className="h-12 w-12 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
+      <li>
+        <div className="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out">
+          <div className="flex px-4 py-4 sm:px-6">
+            <div className="flex-shrink-0 mr-6">
+              <img
+                className="h-12 w-12 rounded-full"
+                src={this.props.avatar_url}
+                alt=""
+              />
+            </div>
+            <div className="flex-auto">
+              <div className="flex items-center justify-between">
+                <span className="text-lg leading-5 font-medium text-indigo-600 truncate">
+                  {this.props.name}
+                </span>
+                <div className="ml-2 flex-shrink-0 flex">
+                  <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                    {this.props.public_repos} Public repos
+                  </span>
+                </div>
               </div>
-              <div className="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
-                <div>
-                  <div className="text-sm leading-5 font-medium text-indigo-600 truncate">
-                    Ricardo Cooper
+              <div className="mt-2 sm:flex sm:justify-between">
+                <div className="sm:flex">
+                  <div className="mr-6 flex items-center text-sm leading-5 text-gray-500">
+                    <svg
+                      className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
+                    </svg>
+                    {this.props.followers} Followers
                   </div>
-                  <div className="mt-2 flex items-center text-sm leading-5 text-gray-500">
+                  <a
+                    href={this.props.html_url}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="mt-2 flex items-center text-sm leading-5 text-blue-500 sm:mt-0"
+                  >
                     <svg
                       className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
                       fill="currentColor"
@@ -30,53 +50,37 @@ export class GithubItem extends Component {
                     >
                       <path
                         fillRule="evenodd"
-                        d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884zM18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"
+                        d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
                         clipRule="evenodd"
                       />
                     </svg>
-                    <span className="truncate">ricardo.cooper@example.com</span>
-                  </div>
+                    Github account
+                  </a>
                 </div>
-                <div className="hidden md:block">
-                  <div>
-                    <div className="text-sm leading-5 text-gray-900">
-                      Applied on
-                      <time datetime="2020-01-07">January 7, 2020</time>
-                    </div>
-                    <div className="mt-2 flex items-center text-sm leading-5 text-gray-500">
-                      <svg
-                        className="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                      Completed phone screening
-                    </div>
-                  </div>
+                <div className="mt-2 flex items-center text-sm leading-5 text-gray-500 sm:mt-0">
+                  <svg
+                    className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <span>
+                    <span className="mr-1">Join</span>
+                    <time dateTime={this.props.created_at}>
+                      <Moment fromNow>{this.props.created_at}</Moment>
+                    </time>
+                  </span>
                 </div>
               </div>
             </div>
-            <div>
-              <svg
-                className="h-5 w-5 text-gray-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
           </div>
-        </a>
-      </div>
+        </div>
+      </li>
     );
   }
 }
