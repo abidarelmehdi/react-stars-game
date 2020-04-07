ffd57ed08e55dd7df2f078d5f666389d12c3619c
Abidar El Mehdi <abidar.mehdi95@gmail.com>
2020-04-07 22:59:06 +0100

    Updates

---
+++ precache-manifest.4c3789ce16187ae1f8c2a44c8980698b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a99fdd2452719c190c0ac5d5038480",
+    "url": "/react-stars-game/index.html"
+  },
+  {
+    "revision": "4375ef910dcc4972d8f6",
+    "url": "/react-stars-game/static/css/main.80d2a842.chunk.css"
+  },
+  {
+    "revision": "9617ae457cbacf5fac22",
+    "url": "/react-stars-game/static/js/2.78d13d9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-stars-game/static/js/2.78d13d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4375ef910dcc4972d8f6",
+    "url": "/react-stars-game/static/js/main.42d217ec.chunk.js"
+  },
+  {
+    "revision": "e4ecbac4bbc6adb2448f",
+    "url": "/react-stars-game/static/js/runtime-main.614f6c59.js"
+  }
+]);
